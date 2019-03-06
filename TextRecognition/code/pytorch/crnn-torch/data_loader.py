4d7a1134a5f22688c62e8c7767396e0c83b10a8a
Sung Man Cho <pccho9108@gmail.com>
2019-03-07 00:38:49 +0900

    [190307] crnn-torch upload

---
+++ TextRecognition/code/pytorch/crnn-torch/data_loader.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+
+
