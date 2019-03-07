e05d2481b27d46c80c88f85d7041417191187dd6
Sung Man Cho <pccho9108@gmail.com>
2019-03-07 23:53:04 +0900

    data loader, train edit

--- TextRecognition/code/pytorch/crnn/train.py
+++ TextRecognition/code/pytorch/crnn/train.py
@@ -1,15 +1,48 @@
-import os
 import torch
-from torch import nn
-from torch import optim
-from torch.utils.data import DataLoader
-from torchvision import transforms, Compose
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
 
 
-def main():
+def parse_arguments(argv)
+    parser = argparse.ArgumentParser(description='CRNN pytorch')
+    
+    # data configurations
+    parser.add_argument('--dataroot', type=str, default=None, help='Path to dataset')
 
+    # input image size
+    parser.add_argument('--input_size', type=str, default="320x32", help='Input size')
 
+    # batch size
+    parser.add_argument('--batch_size', type=int, default=256, help='Batch size')
 
+    return parser.parse_args(argv)
+
+def main(args):
+    # argument handling
+    input_size = [int(x) for x in args.input_size.split('x')]
+
+    # random seed
+    random.seed(random.randint(1, 10000))
+
+    # for faster training
+    cudnn.banchmark = True
+
+    # train transformation
+    transform = transforms.Compose([
+        transforms.Resize(size=[input_size[0], input_size[1]])
+        ])
+
+    # train dataset
+    data = CrnnDataLoader(data_path=args.dataroot, mode="train", transform=transform)
 
 if __name__ == '__main__':
-    main()
+    main(parse_arguments(sys.argv[1:]))
+
